c6a5e18f3c110c794893ef65c3394bbec21eecde
Richard Samuelson <noobtoothfairy@gmail.com>
2017-09-18 21:51:29 +0200

    fixed the dependency issue when making a fresh installation: accidently imported an old css file which is ignored by .gitignore and not in the repo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './styles/index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
